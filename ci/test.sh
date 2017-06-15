bd9f8f36ac1d742bf0ba43271d26e9e71d471860
Michon van Dooren <michon1992@gmail.com>
2017-06-15 16:34:40 +0200

    Documented the dependency on sed

--- ci/test.sh
+++ ci/test.sh
@@ -3,48 +3,64 @@
 set -e errexit -e pipefail -e nounset
 
 apk add --no-cache \
-    bats \
-    loksh mksh zsh \
-    gawk
-apk add --no-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted \
+    bats loksh mksh zsh gawk sed
+apk add --no-cache \
+    --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ \
+    --allow-untrusted \
     dash
 
+declare -A SHELLS AWKS SEDS
 SHELLS=(
-    "/bin/busybox sh"
-    /bin/bash
-    "/bin/bash --posix"
-    /bin/dash
-    /bin/loksh
-    /bin/mksh
-    /bin/zsh
+    [busybox sh]="/bin/busybox sh"
+    [bash]=/bin/bash
+    [bash-posix]="/bin/bash --posix"
+    [dash]=/bin/dash
+    [loksh]=/bin/loksh
+    [mksh]=/bin/mksh
+    [zsh]=/bin/zsh
 )
+DEPENDENCIES=(awk sed)
 AWKS=(
-    "/bin/busybox awk"
-    /usr/bin/gawk
+    [busybox awk]="/bin/busybox awk"
+    [gnu awk]=/usr/bin/gawk
+)
+SEDS=(
+    [busybox sed]="/bin/busybox sed"
+    [gnu sed]=/bin/sed
 )
 
 failed=0
 
-echo
-echo "== Shells"
-for BATS_SHELL in "${SHELLS[@]}"; do
-    echo
-    echo "=== $BATS_SHELL"
-    echo
-    bats tests || failed=1
-done
+export ASCIIDOC_UTILS_PATH="$PWD/src"
 
 echo
-echo "== Awks"
-mkdir -p bin
-for awk in "${AWKS[@]}"; do
+echo "== Shells"
+for shell_name in "${!SHELLS[@]}"; do
     echo
-    echo "=== $awk"
+    echo "=== $shell_name"
     echo
-    echo "#!/bin/sh" > bin/awk
-    echo "$awk "'"$@"' >> bin/awk
-    chmod +x bin/awk
-    PATH="$PWD/bin:$PATH" bats tests || failed=1
+    BATS_SHELL="${SHELLS[$shell_name]}"
+    bats tests || failed=1
+done
+unset BATS_SHELL
+
+for dep in "${DEPENDENCIES[@]}"; do
+    echo
+    echo "== \`$dep\`"
+    mkdir -p bin
+    varname="$(echo "${dep}s" | tr /a-z/ /A-Z/)"
+    eval "$(declare -p $varname | sed "s/$varname/TEMP/")"
+    for prog_name in "${!TEMP[@]}"; do
+        echo
+        echo "=== $prog_name"
+        echo
+        prog="${TEMP[$prog_name]}"
+        echo "#!/bin/sh" > bin/$dep
+        echo "$prog "'"$@"' >> bin/$dep
+        chmod +x bin/$dep
+        PATH="$PWD/bin:$PATH" bats tests || failed=1
+    done
+    rm -rf bin
 done
 
 exit $failed
